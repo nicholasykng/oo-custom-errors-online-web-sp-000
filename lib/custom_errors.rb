5260fa9dac094af50f1b8b0d3c8b151f2d0a46b8
Nicholas Ng <nicholasykng@gmail.com>
2019-11-03 03:11:15 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,11 +7,15 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class != Person
+      raise PartnerError
+    else
     person.partner = self
   end
-
 end
-
+class PartnerError < StandardError
+end
+end
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
